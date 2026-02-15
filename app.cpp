1e56ed70b9a0c26835a4fa4439280a757697593c
Mohamed Amr <68033172+MohamedAmr26@users.noreply.github.com>
2026-02-15 16:40:30 +0000

    refactoring into an organized, oop friendly architecture

--- app.cpp
+++ app.cpp
@@ -10,7 +10,20 @@
 
 using namespace std;
 
-class ConsoleUI {
+class UI{
+public:
+    virtual void showWelcome() = 0;
+    virtual void askLogin(string& username, string& password) = 0;
+    virtual void showInvalidLogin() = 0;
+    virtual int askMainMenu(bool isAdmin) = 0;
+    virtual void showBooks(const vector<string>& books) = 0;
+    virtual int askBookChoose(int size) = 0;
+    virtual int AskBookSessions(const string& book_name, const vector<string>& session_strings, const vector<int>& session_ids) = 0;
+    virtual void displayProfile(string username, bool isAdmin, int sessionCount) = 0;
+    virtual int askUserSessions(const vector<string>& session_strings) = 0;
+};
+
+class ConsoleUI : public UI {
 public:
     void showWelcome() {
         cout << "Welcome to Online Book Reader!" << endl;
@@ -64,65 +77,61 @@ public:
         }
         return choice;
     }
-    int AskBookSessions(const string& book_name, User& user) {
+    int AskBookSessions(const string& book_name, const vector<string>& session_strings, const vector<int>& session_ids){
         cout << "\nReading Sessions for \"" << book_name << "\":\n";
-        vector<const Session*> sessions = user.get_sessions_for(book_name);
-        for (int i = 0; i < sessions.size(); ++i) {
-            cout << (i + 1) << "- " << sessions[i]->toString() << endl;
+        for (int i = 0; i < session_strings.size(); ++i) {
+            cout << (i + 1) << "- " << session_strings[i] << endl;
         }
 
         int choice;
-        cout << sessions.size()+1 << "- " << "Create new Session\n";
+        cout << session_strings.size()+1 << "- " << "Create new Session\n";
         cin >> choice;
-        if (choice <= sessions.size()){
-            return sessions[choice-1]->get_id();
+        if (choice <= session_strings.size()){
+            return session_ids[choice-1]; // Return session ID of selected session
         }
         return -1;
     }
 
-    void displayProfile(const User& user){
-        cout << "Username: " << user.get_username() << "\n" << "Admin: " << user.is_admin() << "\n"
-        << "Number of book sessions: " << user.sessionsSize() << endl;
-        cout << "(1-9)- Return to Main Menu\n";
+    void displayProfile(string username, bool isAdmin, int sessionCount) {
+        cout << "Username: " << username << "\n" << "Admin: " << isAdmin << "\n"
+        << "Number of book sessions: " << sessionCount << endl;
+        cout << "(0-9)- Return to Main Menu\n";
         int c;
         cin >> c;
     }
-    int askUserSessions(const User& user){
+    int askUserSessions(const vector<string>& session_strings){
         cout << "\nYour Reading Sessions:\n";
-        const vector<Session>& sessions = user.get_sessions();
-        int i;
-        for (i = 0; i < sessions.size(); ++i) {
-            cout << i+1 << " - " << sessions[i].toString() << endl;
+
+        for (int i = 0; i < session_strings.size(); ++i) {
+            cout << i+1 << " - " << session_strings[i] << endl;
         }
-        cout << sessions.size()+1 <<"- Return to Main Menu\n";
+        cout << session_strings.size()+1 <<"- Return to Main Menu\n";
+
         int c;
         cin >> c;
-        if (c <= sessions.size()){
-            return sessions[c-1].get_id();
+        if (c <= session_strings.size()){
+            return c-1; // Return index of selected session
         }
         return -1;
     }
 };
 
-User my_user;
-Library library;
-ConsoleUI UI;
+class AuthenticationService {
+    private:
+        static User* user;
+    public:
+        static bool login(string &username, string &password){
+            return user->login(username, password);
+        }
+        static void setUser(User* u) {
+            user = u;
+        }
+};
 
-void loginUser(){
-    string username;
-    string password;
+User* AuthenticationService::user = new User(); // why cannot do it inside main?
 
-    UI.showWelcome();
-    UI.askLogin(username, password);
-
-    while (!my_user.login(username, password)){
-        UI.showInvalidLogin();
-        UI.askLogin(username, password);
-    }
-}
-
-void main_menu_runner(){
-    int current_choice = UI.askMainMenu(my_user.is_admin());
+void main_menu_runner(UI* ui, Library& library){
+    int current_choice = ui->askMainMenu(my_user.is_admin());
 
     if (!my_user.is_admin()){
         switch (current_choice){
@@ -201,6 +210,16 @@ void main_menu_runner(){
     }
 }
 
+void loginUser(UI* ui){
+    string username, password;
+    ui->askLogin(username, password);
+
+    while (!AuthenticationService::login(username, password)){
+        ui->showInvalidLogin();
+        ui->askLogin(username, password);
+    }
+}
+
 int main(){
     map<string, pair<string, bool> > accounts;
     accounts["mohamedAmr26"] = make_pair("examplePassword1", false);
@@ -209,6 +228,9 @@ int main(){
 
     User::load_accounts(accounts);
 
+    Library library;
+    UI* app_ui = new ConsoleUI();
+
     vector<string> pages(3);
     pages[0] = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
     pages[1] = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
@@ -216,6 +238,6 @@ int main(){
     Book mohamed_iwl("Mohamed in Wonderland", "SussyBaka", pages);
     library.insert_book(mohamed_iwl);
 
-    loginUser();
-    main_menu_runner();
+    loginUser(app_ui);
+    main_menu_runner(app_ui, library);
 }
